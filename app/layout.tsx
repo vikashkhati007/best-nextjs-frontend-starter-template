1aaef84290c52850a878041bd3522c47a10b9fb8
Vikash Khati <vikashkhati007@gmail.com>
2024-11-02 20:02:59 +0530

    firstcommit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import clsx from "clsx";
 import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
+import { fontSans, myFont } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
@@ -42,23 +42,9 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className={clsx("container mx-auto max-w-7xl pt-16 px-6 flex-grow", myFont.variable)}>
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
-            </footer>
-          </div>
         </Providers>
       </body>
     </html>
--- config/fonts.ts
+++ config/fonts.ts
@@ -1,4 +1,17 @@
 import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import localFont from 'next/font/local';
+
+// Define your font configuration
+export const myFont = localFont({
+  src: [
+    {
+      path: '../public/fonts/Ginto-400.woff', // Path to your WOFF file
+      weight: '400', // Normal weight
+      style: 'normal', // Normal style
+    },
+  ],
+  variable: '--my-custom-font', // CSS variable for use in styles
+});
 
 export const fontSans = FontSans({
   subsets: ["latin"],
