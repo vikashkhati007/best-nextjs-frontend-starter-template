90ecf0cf6cb062489011984213337c63e0d3e449
Vikash Khati <vikashkhati007@gmail.com>
2024-11-06 13:33:26 +0530

    aichatbot

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import { handlegetChats } from "@/actions/handlegetChats";
+import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 
 export default function Home() {
+const handleSubmit = async (formData: FormData) => {
+  "use server"
+  const prompt: any = formData.get("prompt");
+  const res = await handlegetChats(prompt);
+  console.log(res);
+}
   return (
-    <div >
+    <form action={handleSubmit}>
      Full Name
-     <Input placeholder="Enter Your Full Name" />
-    </div>
+     <Input placeholder="Enter Your Prompt" name="prompt"  />
+     <Button type="submit">Submit</Button>
+    </form>
   );
 }
